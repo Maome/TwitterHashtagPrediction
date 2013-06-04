d62922d467d446a4a488a6fe1991b07284b7238b
Jon <syrok77@gmail.com>
2013-06-03 22:16:18 -0700

    did some stuff.

--- parse.py
+++ parse.py
@@ -1,12 +1,14 @@
 import json
 
-myfile = file("twitterdata.txt")
-outfile = open('parsed.txt', 'r+')
+#https://stream.twitter.com/1.1/statuses/sample.json?language=en
+
+myfile = file("../temp.txt")
+outfile = open('../parsed.txt', 'r+')
 
 for line in myfile:
     data = json.loads(line)
     if data['text'][0:2] != "RT" and data['text'][0:1] != '@' and data['entities']['hashtags'] != []:
-        print data['text']#, "#%s" % data['entities']['hashtags'][0]['text']
+        #print data['text']#, "#%s" % data['entities']['hashtags'][0]['text']
         outfile.write(line)
 
 outfile.close()
